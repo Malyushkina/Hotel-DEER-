a08fbbde92e76406907499724cff16f79dcff71a
Evgeniya Malyushkina <e.malyushkina@mail.ru>
2020-10-09 15:30:25 +0800

    Поставила календарь

--- source/js/main.js
+++ source/js/main.js
@@ -12,3 +12,17 @@ navToggle.addEventListener('click', function () {
     navMain.classList.remove('nav--opened');
   }
 });
+
+function validate() {
+  var date = new Date();
+  var currDate = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
+  dateIn = document.getElementById("check-in").value;
+  console.log(dateIn)
+  var dateOut = document.getElementById("departure").value;
+  // if (dateIn < currDate || dateOut < currDate) {
+  //   console.log("Дата меньше текущей")
+  // }
+  // if(dateIn>dateOut){
+  //   console.log("Дата заезда больше даты выезда")
+  // }
+}
